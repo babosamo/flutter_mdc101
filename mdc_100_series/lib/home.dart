e37dc6f2daa4ae23ec803d34d87aaeff69ae0ec0
noah <noah.kim@kakaocorp.com>
2019-05-12 23:59:28 +0900

    104# done

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,20 +13,20 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import 'model/products_repository.dart';
 import 'model/product.dart';
 import 'supplemental/asymmetric_view.dart';
 
-
-
 class HomePage extends StatelessWidget {
-
   // TODO: Add a variable for Category (104)
+  final Category category;
+
+  const HomePage({this.category: Category.all});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: Return an AsymmetricView (104)
-    return  AsymmetricView(products: ProductsRepository.loadProducts(Category.all));
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return AsymmetricView(products: ProductsRepository.loadProducts(category));
   }
 }
